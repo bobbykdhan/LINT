6ae30e7deffad70f46046ec73a7f2020d1b66b24
Wilson McDade <wilson.mcdade@gmail.com>
2023-09-06 23:45:16 -0400

    Added form submission

--- src/app.py
+++ src/app.py
@@ -1,19 +1,19 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__)
 
-@app.route("/")
-async def root():
-    return {"message": "Hello World"}
+validBuildingNames = ["Gleason", "Sol", "Gibson", "Ellingson"]
 
-
-@app.route("/hello/<name>")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.route("/ticket/<building>/<machineNum>")
-async def ticket(building, machineNum):
-    return flask.render_template("ticket.html")
+@app.route("/ticket", methods=["GET", "POST"])
+async def ticket():
+    if request.method == "GET":
+        building = request.args.get('building')
+        machineNum = request.args.get('machineNum')
+        return render_template("ticket.html", building=building, machineNum=machineNum)
+    else:
+        try:
+            return "Thanks!", 200
+        except ValueError as value_err:
+            return str(value_err), 400
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
